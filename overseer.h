0f5da6b60bc3859b92eb05589396cff852d04df9
Ryan Tucker <rtucker@gmail.com>
2014-07-26 15:04:56 +0000

    Allow display of longer strings

--- overseer.h
+++ overseer.h
@@ -19,12 +19,12 @@ struct sensor_s
 
 typedef struct sensor_s sensor_t;
 
-#define MAX_FORECAST_WIDTH 100
+#define MAX_FORECAST_WIDTH 512
 #define FORECAST_NOW "/dev/shm/iface_forecastio.now.txt"
 #define FORECAST_LATER "/dev/shm/iface_forecastio.later.txt"
 
-#define MAX_OUTBUF 255
+#define MAX_OUTBUF 1024
 
 #define MUNIN_FILE "/dev/shm/overseer_reading"
 
-#endif
+#endif // __INCLUDE_OVERSEER_H_
